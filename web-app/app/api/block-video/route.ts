95d13cce261aa5d3f0ad070cb1a38559c87504c9
Sujal Chauhan <chauhansujal1107@gmail.com>
2025-04-10 10:11:44 +0530

    api changes

--- web-app/app/api/block-video/route.ts
+++ web-app/app/api/block-video/route.ts
@@ -1,15 +1,23 @@
 import {  NextResponse } from 'next/server';
 import { db } from "@/db/drizzle";
 import { videos } from "@/db/video";
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
+import { auth } from '@clerk/nextjs/server';
 export async function POST(req: Request){
     const { videoURL } = await req.json();
-   
+    const { userId } = await auth();
+              
+    const user = await fetch(`https://api.clerk.dev/v1/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+      },
+    }).then(res => res.json());
+    const userEmail = user?.email_addresses?.[0]?.email_address;
       try {
        await db
         .update(videos)
         .set({isBlocked: true })
-        .where(eq(videos.url, videoURL)) 
+        .where(and(eq(videos.url, videoURL), eq(videos.userEmail, userEmail ))) 
         .execute();
         
         
--- web-app/app/api/delete-video/route.ts
+++ web-app/app/api/delete-video/route.ts
@@ -1,17 +1,25 @@
 import { db } from '@/db/drizzle';
 import { videos } from '@/db/video';
-import { eq } from 'drizzle-orm';
+import { auth } from '@clerk/nextjs/server';
+import { and, eq } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
 export async function DELETE(request: Request) {
   try {
     const { id } = await request.json();
-
+    const { userId } = await auth();
+              
+    const user = await fetch(`https://api.clerk.dev/v1/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+      },
+    }).then(res => res.json());
+    const userEmail = user?.email_addresses?.[0]?.email_address;
     if (!id) {
       return NextResponse.json({ error: 'Video ID is required' }, { status: 400 });
     }
 
-    const result = await db.delete(videos).where(eq(videos.id, id));
+    await db.delete(videos).where(and(eq(videos.id, id), eq(videos.userEmail, userEmail)));
 
     return NextResponse.json({ message: 'Video deleted successfully' });
   } catch (err) {
