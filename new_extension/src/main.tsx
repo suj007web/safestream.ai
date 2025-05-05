a091960d1dfb6478e6bccc725269da8d55b33c0a
Sujal Chauhan <chauhansujal1107@gmail.com>
2025-05-05 21:28:39 +0530

    added router

--- new_extension/src/main.tsx
+++ new_extension/src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import App from './App.tsx'
+import { SignIn, useAuth } from '@clerk/clerk-react';
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const { isSignedIn, isLoaded } = useAuth();
+  
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  
+  if (!isSignedIn) {
+    return <Navigate to="/sign-in" />;
+  
+  }
+  return <>{children}</>;
+};
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+      <Route path="/" element={
+        <ProtectedRoute>
+        <App />
+      </ProtectedRoute>
+      } />
+      <Route path="/sign-in/*" element={<SignIn routing="path" path="/sign-in" />} />
+    </Routes>
+</BrowserRouter>
+  
+  
 )
